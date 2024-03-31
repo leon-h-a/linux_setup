4026c7d9241086db026a1490d57ee570e0b8cdb7
leon-ha <lekileo@gmail.com>
2024-04-01 01:02:48 +0200

    feat: add lua func for deleting latex compilation files

--- nvim/lua/leon/tex.lua
+++ nvim/lua/leon/tex.lua
@@ -13,6 +13,16 @@ vim.keymap.set("n", "<F9>", [[
     :! rm *.fdb_latexmk
 ]], { silent = true })
 
+vim.keymap.set("n", "afa", function()
+    local ffp = vim.api.nvim_buf_get_name(0)
+    local base = ffp:match("(.+)%..+$")
+    local ext = {".aux", ".out", ".log", ".fls", ".synctex.gz", ".fdb_latexmk"}
+    for i, v in pairs(ext) do
+        os.remove(base .. v)
+    end
+    print("LaTeX compilation files deleted!")
+end)
+
 vim.api.nvim_create_autocmd({"BufEnter"}, {
     pattern = "*.tex",
     callback = function()
