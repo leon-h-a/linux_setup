7b4fdb70a7b536268f5d9f0b638d2eee5fb8d680
leon-ha <lekileo@gmail.com>
2024-04-12 22:57:01 +0200

    feat: add cpplint to lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,32 +1,50 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({buffer = bufnr})
 end)
+
+local handlers = {
+    ["lua_ls"] = function ()
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' }
+                    }
+                }
+            }
+        }
+    end,
+    ["pylsp"] = function ()
+        local lspconfig = require("lspconfig")
+        lspconfig.pylsp.setup {
+            settings = {
+                pylsp = {
+                    plugins = {
+                        pycodestyle = {
+                            maxLineLength = "81"
+                        }
+                    }
+                }
+            }
+        }
+    end
+}
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {
-		"lua_ls",
-		"pylsp"
-	},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+    ensure_installed = {
+        "lua_ls",
+        "pylsp",
+        "cpplint"
+    },
+    automatic_installation = true,
+    handlers = handlers
 })
 
--- Languages
-require("lspconfig").lua_ls.setup {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-}
-require("lspconfig").pylsp.setup {}
