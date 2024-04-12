d5ef712f6415d4e5f48dee7d59edf6ec7b34b226
leon-ha <lekileo@gmail.com>
2024-04-12 21:29:47 +0200

    fix(latex): keymaps for small/med/big skips

--- nvim/lua/leon/tex.lua
+++ nvim/lua/leon/tex.lua
@@ -40,13 +40,13 @@ end)
 vim.keymap.set("n", "<leader>ms", function()
   local pos = vim.api.nvim_win_get_cursor(0)[2]
   local line = vim.api.nvim_get_current_line()
-  local nline = line:sub(0, pos) .. [[\bigskip]] .. line:sub(pos + 1)
+  local nline = line:sub(0, pos) .. [[\medskip]] .. line:sub(pos + 1)
   vim.api.nvim_set_current_line(nline)
 end)
 
 vim.keymap.set("n", "<leader>ss", function()
   local pos = vim.api.nvim_win_get_cursor(0)[2]
   local line = vim.api.nvim_get_current_line()
-  local nline = line:sub(0, pos) .. [[\bigskip]] .. line:sub(pos + 1)
+  local nline = line:sub(0, pos) .. [[\smallskip]] .. line:sub(pos + 1)
   vim.api.nvim_set_current_line(nline)
 end)
